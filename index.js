8c494db8713e9b852f87b76a441fa1f843ce1986
NeoPhetoane <phetoaneneo4@gmail.com>
2024-04-12 15:38:38 +0200

    Code complete: type... catch added

--- index.js
+++ index.js
@@ -1,70 +1,79 @@
-const res = await fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=roses"
-)
-const data = await res.json()
-    console.log(data.urls.regular);
-    document.body.style.backgroundImage = `url(${data.urls.regular})`; //Remember to change to full
+document.addEventListener("DOMContentLoaded", async function () {
+  //Background image
+  try {
+    const res = await fetch(
+      "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=roses"
+    );
+    const data = await res.json();
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
     document.getElementById("author").textContent = `By: ${data.user.name}`;
-
-  // .catch(err => {
-  //   document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4MzQyNjJ8&ixlib=rb-4.0.3&q=80&w=1080)`;
-  //   document.getElementById("author").textContent = `By: ${data.user.name}`;
-  // });
-
-  const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-  if (!res.ok) {
-    throw Error("Something went wrong");
+  } catch (err) {
+    //Alternative Image, incase the image does not load
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4MzQyNjJ8&ixlib=rb-4.0.3&q=80&w=1080)`;
+    document.getElementById("author").textContent = "By: Oscar Helgstrand";
   }
-  const data = await res.json()
-  
+
+  //Crypto
+
+  //Fetches data from Dogecoin API
+  try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin");
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    const data = await res.json();
+    //Logo and name
     document.getElementById("crypto-top").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
         `;
+    //Data displaay
     document.getElementById("crypto").innerHTML += `
         <p>🎯:R${data.market_data.current_price.zar}</p>
         <p>⬆️:R${data.market_data.high_24h.zar}</p>
         <p>⬇️:R${data.market_data.low_24h.zar}</p>
         
         `;
+  } catch (err) {
+    console.error(err);
+  }
 
-  .catch((err) => console.error(err));
+  //Time display
 
-//Time display
+  function getCurrentTime() {
+    const date = new Date();
+    document.getElementById("time").textContent = date.toLocaleTimeString(
+      "en-us",
+      { timeStyle: "short" }
+    );
+  }
 
-function getCurrentTime() {
-  const date = new Date();
-  document.getElementById("time").textContent = date.toLocaleTimeString(
-    "en-ZA",
-    { timeStyle: "short" }
-  );
-}
+  setInterval(getCurrentTime, 1000); //To constantly update the time
 
-setInterval(getCurrentTime, 1000);
+  //Weather
 
-//Weather
-
-//Fetching the weather with location
-navigator.geolocation.getCurrentPosition(async position => {
-  fetch(
-    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
-  )
-    .then((res) => {
+  //Fetching the weather with location
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    try {
+      const res = await fetch(
+        `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+      );
       if (!res.ok) {
         //Checks if the data was available
         throw Error("Weather data not available");
       }
-      return res.json();
-    })
 
-    //Icon and display of temperature
-    .then((data) => {
+      const data = await res.json();
+
+      //Icon and display of temperature
       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.getElementById("weather").innerHTML = `
               <img src=${iconUrl} />
               <p class="weather-temp">${Math.round(data.main.temp)}ºC</p>
               <p class="weather-city">${data.name}</p>
           `;
-    })
-    .catch((err) => console.error(err));
+    } catch (err) {
+      console.error(err);
+    }
+  });
 });
