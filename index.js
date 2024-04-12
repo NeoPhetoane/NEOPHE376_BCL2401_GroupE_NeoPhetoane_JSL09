7073804e4197ba8b70d95eb013529faee143b045
NeoPhetoane <phetoaneneo4@gmail.com>
2024-04-12 14:00:16 +0200

    CSS updated

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-fetch(
+const res = await fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=roses"
 )
-  .then((res) => res.json())
-  .then((data) => {
+const data = await res.json()
     console.log(data.urls.regular);
-    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.body.style.backgroundImage = `url(${data.urls.regular})`; //Remember to change to full
     document.getElementById("author").textContent = `By: ${data.user.name}`;
-  })
-  .catch((err) => {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4MzQyNjJ8&ixlib=rb-4.0.3&q=80&w=1080)`;
-    document.getElementById("author").textContent = `By: ${data.user.name}`;
-  });
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-  .then((res) => {
-    if (!res.ok) {
-      throw Error("Something went wrong");
-    }
-    return res.json();
-  })
-  .then((data) => {
+  // .catch(err => {
+  //   document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4MzQyNjJ8&ixlib=rb-4.0.3&q=80&w=1080)`;
+  //   document.getElementById("author").textContent = `By: ${data.user.name}`;
+  // });
+
+  const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  if (!res.ok) {
+    throw Error("Something went wrong");
+  }
+  const data = await res.json()
+  
     document.getElementById("crypto-top").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
@@ -30,7 +27,7 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         <p>⬇️:R${data.market_data.low_24h.zar}</p>
         
         `;
-  })
+
   .catch((err) => console.error(err));
 
 //Time display
@@ -44,3 +41,30 @@ function getCurrentTime() {
 }
 
 setInterval(getCurrentTime, 1000);
+
+//Weather
+
+//Fetching the weather with location
+navigator.geolocation.getCurrentPosition(async position => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        //Checks if the data was available
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+
+    //Icon and display of temperature
+    .then((data) => {
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
+              <img src=${iconUrl} />
+              <p class="weather-temp">${Math.round(data.main.temp)}ºC</p>
+              <p class="weather-city">${data.name}</p>
+          `;
+    })
+    .catch((err) => console.error(err));
+});
